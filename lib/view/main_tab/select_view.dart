d93a9b36f379ebb05f5ab30420ce71b733c5f5bf
chau4w <21020286@vnu.edu.vn>
2024-04-15 01:01:44 +0700

    fix UI một tí

--- lib/view/main_tab/select_view.dart
+++ lib/view/main_tab/select_view.dart
@@ -17,39 +17,149 @@ class SelectView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            RoundButton(
-                title: "Workout Tracker",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const WorkoutTrackerView(),
+            Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border:
+                      Border.all(color: TColor.primaryColor1.withOpacity(0.5)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: TColor.primaryColor2.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
                     ),
-                  );
-                }),
+                  ],
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WorkoutTrackerView(),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/img/workout.webp',
+                        width: 100,
+                        height: 100,
+                      ),
+                      SizedBox(
+                        width: media.width * 0.05,
+                      ),
+                      const Text(
+                        'Workout Planner',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
             const SizedBox(
               height: 15,
             ),
-            RoundButton(
-                title: "Meal Planner",
-                onPressed: () {
-                  Navigator.push(
+            Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border:
+                      Border.all(color: TColor.primaryColor1.withOpacity(0.5)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: TColor.primaryColor1.withOpacity(0.6),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const MealPlannerView()));
-                  const SizedBox(
-                    height: 15,
-                  );
-                }),
+                        builder: (context) => const MealPlannerView(),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/img/meal.png',
+                        width: 100,
+                        height: 100,
+                      ),
+                      SizedBox(
+                        width: media.width * 0.05,
+                      ),
+                      const Text(
+                        'Meal Planner',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
             const SizedBox(height: 15),
-            RoundButton(
-                title: "Sleep Tracker",
-                onPressed: () {
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border:
+                    Border.all(color: TColor.primaryColor1.withOpacity(0.5)),
+                boxShadow: [
+                  BoxShadow(
+                    color: TColor.primaryColor1.withOpacity(0.7),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: InkWell(
+                onTap: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SleepTrackerView()));
-                }),
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SleepTrackerView(),
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/img/sleep.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: media.width * 0.05,
+                    ),
+                    const Text(
+                      'Sleep Tracker',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
